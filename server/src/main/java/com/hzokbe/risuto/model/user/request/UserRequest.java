d0d6f1ce4313dc1b738e84c7b415e71a9fa40998
hzokbe <hzokbe@gmail.com>
2025-06-29 16:54:38 -0300

    feat(server/model): add UserRequest class

---
+++ server/src/main/java/com/hzokbe/risuto/model/user/request/UserRequest.java
@@ -0,0 +1,23 @@
+package com.hzokbe.risuto.model.user.request;
+
+public class UserRequest {
+    private String username;
+
+    private String rawPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRawPassword() {
+        return rawPassword;
+    }
+
+    public void setRawPassword(String rawPassword) {
+        this.rawPassword = rawPassword;
+    }
+}
