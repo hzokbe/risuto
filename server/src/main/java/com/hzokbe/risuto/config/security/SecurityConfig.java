e74cb327d4810e087fc64dea117efb4066b94ca3
hzokbe <hzokbe@gmail.com>
2025-06-29 17:05:14 -0300

    feat(server/config/security): add 'getSecurityFilterChain' method

--- server/src/main/java/com/hzokbe/risuto/config/security/SecurityConfig.java
+++ server/src/main/java/com/hzokbe/risuto/config/security/SecurityConfig.java
@@ -2,13 +2,27 @@ package com.hzokbe.risuto.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 2);
+    }
+
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity security) throws Exception {
+        return security
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+                r -> r.requestMatchers(HttpMethod.POST, "/sign-up").permitAll().anyRequest().denyAll()
+            )
+            .build();
     }
 }
