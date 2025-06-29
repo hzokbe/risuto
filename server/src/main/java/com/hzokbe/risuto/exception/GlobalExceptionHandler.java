c88e71a718d5efd9125320957e0e4af0b9f0ad16
hzokbe <hzokbe@gmail.com>
2025-06-29 17:58:17 -0300

    feat(server/exception): add 'exceptionHandler' method

--- server/src/main/java/com/hzokbe/risuto/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/hzokbe/risuto/exception/GlobalExceptionHandler.java
@@ -1,7 +1,19 @@
 package com.hzokbe.risuto.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.hzokbe.risuto.model.exception.response.ExceptionResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ExceptionResponse exceptionHandler(Exception exception) {
+        return new ExceptionResponse("internal server error");
+    }
 }
