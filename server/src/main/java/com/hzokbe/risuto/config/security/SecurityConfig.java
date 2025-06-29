06b65b9a135a40c952d790e1f198da8a03340dc9
hzokbe <hzokbe@gmail.com>
2025-06-29 16:45:45 -0300

    feat(server/config/security): add 'getPasswordEncoder' method

--- server/src/main/java/com/hzokbe/risuto/config/security/SecurityConfig.java
+++ server/src/main/java/com/hzokbe/risuto/config/security/SecurityConfig.java
@@ -1,7 +1,14 @@
 package com.hzokbe.risuto.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 2);
+    }
 }
