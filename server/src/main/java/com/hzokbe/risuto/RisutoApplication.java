2105acca175b931b88606fb7777821b1b1de3771
hzokbe <hzokbe@gmail.com>
2025-06-29 16:26:55 -0300

    chore(server): add initial server-side setup

---
+++ server/src/main/java/com/hzokbe/risuto/RisutoApplication.java
@@ -0,0 +1,11 @@
+package com.hzokbe.risuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisutoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RisutoApplication.class, args);
+	}
+}
