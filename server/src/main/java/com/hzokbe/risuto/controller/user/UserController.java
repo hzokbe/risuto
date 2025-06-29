74aa21ef48235fa62eb601b88021e1789a57902d
hzokbe <hzokbe@gmail.com>
2025-06-29 17:01:55 -0300

    feat(server/controller/user): add 'signUp' method

--- server/src/main/java/com/hzokbe/risuto/controller/user/UserController.java
+++ server/src/main/java/com/hzokbe/risuto/controller/user/UserController.java
@@ -1,7 +1,13 @@
 package com.hzokbe.risuto.controller.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hzokbe.risuto.model.user.request.UserRequest;
+import com.hzokbe.risuto.model.user.response.UserResponse;
 import com.hzokbe.risuto.service.user.UserService;
 
 @RestController
@@ -10,5 +16,10 @@ public class UserController {
 
     public UserController(UserService service) {
         this.service = service;
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<UserResponse> signUp(@RequestBody UserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.signUp(request));
     }
 }
