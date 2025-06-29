541b5a8532760fdc49f727e63c165f08b1b5b875
hzokbe <hzokbe@gmail.com>
2025-06-29 20:27:02 -0300

    feat(server/exception): add 'invalidPasswordExceptionHandler' method

--- server/src/main/java/com/hzokbe/risuto/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/hzokbe/risuto/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.hzokbe.risuto.exception.user.InvalidPasswordException;
 import com.hzokbe.risuto.exception.user.InvalidUsernameException;
 import com.hzokbe.risuto.model.exception.response.ExceptionResponse;
 
@@ -22,6 +23,13 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ExceptionResponse invalidUsernameExceptionHandler(InvalidUsernameException exception) {
+        return new ExceptionResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ExceptionResponse invalidPasswordExceptionHandler(InvalidPasswordException exception) {
         return new ExceptionResponse(exception.getMessage());
     }
 }
