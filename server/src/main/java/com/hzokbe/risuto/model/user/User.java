62e0d1b5b7082e0ea2854c5807bf76390d29d343
hzokbe <hzokbe@gmail.com>
2025-06-29 16:29:53 -0300

    feat(server/model): add User class

---
+++ server/src/main/java/com/hzokbe/risuto/model/user/User.java
@@ -0,0 +1,52 @@
+package com.hzokbe.risuto.model.user;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    public User() {
+    }
+
+    public User(String username, String passwordHash) {
+        this.username = username;
+
+        this.passwordHash = passwordHash;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
