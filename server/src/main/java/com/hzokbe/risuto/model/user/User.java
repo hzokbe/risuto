451cef709db2c16b8dc11d6262542e41c2163e9c
hzokbe <hzokbe@gmail.com>
2025-06-29 16:31:51 -0300

    feat(server/model/user): add 'role' attribute

--- server/src/main/java/com/hzokbe/risuto/model/user/User.java
+++ server/src/main/java/com/hzokbe/risuto/model/user/User.java
@@ -1,5 +1,6 @@
 package com.hzokbe.risuto.model.user;
 
+import com.hzokbe.risuto.model.role.Role;
 import jakarta.persistence.*;
 
 import java.util.UUID;
@@ -17,6 +18,10 @@ public class User {
     @Column(nullable = false)
     private String passwordHash;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
     public User() {
     }
 
@@ -24,6 +29,14 @@ public class User {
         this.username = username;
 
         this.passwordHash = passwordHash;
+    }
+
+    public User(String username, String passwordHash, Role role) {
+        this.username = username;
+
+        this.passwordHash = passwordHash;
+
+        this.role = role;
     }
 
     public UUID getId() {
@@ -48,5 +61,13 @@ public class User {
 
     public void setPasswordHash(String passwordHash) {
         this.passwordHash = passwordHash;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }
